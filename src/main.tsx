afacea300ec391daf9d0c0bee9617c4c80d1896f
harikeshpandey <harrypandey121@gmail.com>
2025-12-29 10:14:02 +0530

    Production issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,4 @@
-// src/main.tsx
-import React from 'react';
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
@@ -8,7 +7,7 @@ import { Buffer } from 'buffer';
 window.Buffer = Buffer;
 //@ts-ignore
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
